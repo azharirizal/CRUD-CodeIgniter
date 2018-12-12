190865d459ae1e59c159d49f248b6eadf9fd7714
azharirizal <azharibooksstore@gmail.com>
2018-12-12 13:49:05 +0700

    penambahan tabel admin
    penambahan tabel admin untuk melakukan Login aplikasi

--- db/kampus.sql
+++ db/kampus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 23, 2018 at 11:38 AM
+-- Generation Time: Dec 12, 2018 at 07:47 AM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 5.6.36
 
@@ -25,11 +25,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `profil` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `username`, `password`, `nama`, `profil`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'ADMIN', 'images.png');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `mahasiswa`
 --
 
 CREATE TABLE `mahasiswa` (
-  `id` int(11) NOT NULL,
+  `id_mahasiswa` int(11) NOT NULL,
   `nim` varchar(255) DEFAULT NULL,
   `nama` varchar(255) DEFAULT NULL,
   `kelas` varchar(255) DEFAULT NULL,
@@ -40,36 +61,43 @@ CREATE TABLE `mahasiswa` (
 -- Dumping data for table `mahasiswa`
 --
 
-INSERT INTO `mahasiswa` (`id`, `nim`, `nama`, `kelas`, `jurusan`) VALUES
-(2, '123451', 'Aguzrybudy', 'Siang', 'Tehnik Informatika'),
-(6, '123452', 'Heru', 'Malam', 'Sistem Komputer'),
-(7, '123453', 'Ahmadul', 'Karyawan', 'Tehnik Informatika'),
-(8, '123454', 'Ardiansyah', 'Siang', 'Tehnik Informatika'),
-(9, '123455', 'Roy Royandi', 'Malam', 'Sistem Komputer'),
-(10, '123456', 'Ahyar', 'Pagi', 'Tehnik Informatika'),
-(11, '12145484', 'Muhamad Rizal Azhari', 'Karyawan', 'Sistem Informasi');
+INSERT INTO `mahasiswa` (`id_mahasiswa`, `nim`, `nama`, `kelas`, `jurusan`) VALUES
+(16, '12145484', 'Muhamad Kurnia', 'Pagi', 'Teknik Informatika');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
 -- Indexes for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_mahasiswa`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_mahasiswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
